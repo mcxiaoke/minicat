84125363d4711760a339e1d0612a16336ba039d2
mcxiaoke <mcxiaoke@gmail.com>
2012-01-30 10:37:45 +0800

    add pulltorefresh lib

---
+++ pulltorefresh/src/com/handmark/pulltorefresh/library/PullToRefreshExpandableListView.java
@@ -0,0 +1,55 @@
+package com.handmark.pulltorefresh.library;
+
+import com.handmark.pulltorefresh.library.internal.EmptyViewMethodAccessor;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ExpandableListView;
+
+public class PullToRefreshExpandableListView extends PullToRefreshAdapterViewBase<ExpandableListView> {
+
+	class InternalExpandableListView extends ExpandableListView implements EmptyViewMethodAccessor {
+
+		public InternalExpandableListView(Context context, AttributeSet attrs) {
+			super(context, attrs);
+		}
+
+		@Override
+		public void setEmptyView(View emptyView) {
+			PullToRefreshExpandableListView.this.setEmptyView(emptyView);
+		}
+
+		@Override
+		public void setEmptyViewInternal(View emptyView) {
+			super.setEmptyView(emptyView);
+		}
+
+		public ContextMenuInfo getContextMenuInfo() {
+			return super.getContextMenuInfo();
+		}
+	}
+
+	public PullToRefreshExpandableListView(Context context) {
+		super(context);
+	}
+
+	public PullToRefreshExpandableListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected final ExpandableListView createRefreshableView(Context context, AttributeSet attrs) {
+		ExpandableListView lv = new InternalExpandableListView(context, attrs);
+
+		// Set it to this so it can be used in ListActivity/ListFragment
+		lv.setId(android.R.id.list);
+		return lv;
+	}
+
+	@Override
+	public ContextMenuInfo getContextMenuInfo() {
+		return ((InternalExpandableListView)getRefreshableView()).getContextMenuInfo();
+	}
+}
