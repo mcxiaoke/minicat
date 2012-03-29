222da8e2e3ee800012cee08c91d1cece05d1767a
mcxiaoke <mcxiaoke@gmail.com>
2012-03-29 17:54:04 +0800

    fanfou-android: modify some color/style names, need more modification

--- androidapp/src/com/fanfou/app/hd/adapter/BaseStatusArrayAdapter.java
+++ androidapp/src/com/fanfou/app/hd/adapter/BaseStatusArrayAdapter.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
