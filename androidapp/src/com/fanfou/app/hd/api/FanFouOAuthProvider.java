649d01eae42d7ad049873fb9b00edef2b59f0f27
mcxiaoke <mcxiaoke@gmail.com>
2012-05-18 11:05:13 +0800

    modify consumer key

--- androidapp/src/com/fanfou/app/hd/api/FanFouOAuthProvider.java
+++ androidapp/src/com/fanfou/app/hd/api/FanFouOAuthProvider.java
@@ -21,8 +21,8 @@ class FanFouOAuthProvider implements OAuthProvider {
 	// "1EABD7AA1DD250E7981D6D9CE7E07DC907645E76ABEBF14B4E524F6D36597D3A";
 	// public static final String CONSUMER_SECRET =
 	// "ECF318CA13B0E89CF847BC48BCBAECD64082AC3B7FE78C8D21B4FB5D98861344";
-	private static final String CONSUMER_KEY = "c403d5a51bde9cce58fe31f4cec06b0a";
-	private static final String CONSUMER_SECRET = "5da0bcda353f7d2fe8e3de01e3c97741";
+	private static final String CONSUMER_KEY = "84d8fa6c95047475c9a14aae1f53f7b2";
+	private static final String CONSUMER_SECRET = "4623b62a6d6a911533093987406549d0";
 	private static final String REQUEST_TOKEN_URL="http://fanfou.com/oauth/request_token";
 	private static final String AUTHORIZE_URL="http://fanfou.com/oauth/authorize";
 	private static final String ACCESS_TOKEN_URL="http://fanfou.com/oauth/access_token";
