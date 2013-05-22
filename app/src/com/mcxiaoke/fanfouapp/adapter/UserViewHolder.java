fdbdb03f169340a16813dadc254cb8c97839944a
zhangxiaoke <zhangxiaoke@douban.com>
2013-05-22 10:34:08 +0800

    add follow button for userlist

--- app/src/com/mcxiaoke/fanfouapp/adapter/UserViewHolder.java
+++ app/src/com/mcxiaoke/fanfouapp/adapter/UserViewHolder.java
@@ -2,7 +2,6 @@ package com.mcxiaoke.fanfouapp.adapter;
 
 import android.content.res.Resources;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.mcxiaoke.fanfouapp.R;
@@ -20,14 +19,14 @@ public class UserViewHolder {
     public TextView title;
     public ImageView lock;
     public TextView text;
-    public Button button;
+    public TextView button;
 
     public UserViewHolder(View base) {
         head = (ImageView) base.findViewById(R.id.image);
         title = (TextView) base.findViewById(R.id.title);
         lock = (ImageView) base.findViewById(R.id.lock);
         text = (TextView) base.findViewById(R.id.text);
-        button = (Button) base.findViewById(R.id.button);
+        button = (TextView) base.findViewById(R.id.button);
     }
 
     public void setUserContent(Resources res, final UserModel u) {
@@ -36,6 +35,11 @@ public class UserViewHolder {
         text.setText(u.getDescription());
         button.setText(u.isFollowing() ? "正在关注" : "添加关注");
         button.setTextColor(u.isFollowing() ? res.getColorStateList(R.color.text_color_selectable_primary_light_inverse) : res.getColorStateList(R.color.text_color_selectable_primary_light));
-        button.setBackgroundResource(u.isFollowing() ? R.drawable.follow_state_following : R.drawable.follow_state_notfollowing);
+        button.setBackgroundResource(u.isFollowing() ? R.drawable.button_follow_on : R.drawable.button_follow_off);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 }
