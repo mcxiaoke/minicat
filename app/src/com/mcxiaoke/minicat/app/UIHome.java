e335cc603130d8065453f41a7482c8c8e60d2ec7
mcxiaoke <mcxiaoke@gmail.com>
2014-12-01 14:37:26 +0800

    tweak page indicator

--- app/src/com/mcxiaoke/minicat/app/UIHome.java
+++ app/src/com/mcxiaoke/minicat/app/UIHome.java
@@ -251,9 +251,9 @@ public class UIHome extends UIBaseSupport implements MenuCallback,
         final int highlightColor = getResources().getColor(R.color.holo_secondary);
         mPagerTabStrip = (PagerTabStrip) findViewById(R.id.viewpager_strip);
         mPagerTabStrip.setBackgroundResource(R.color.background_secondary);
-        mPagerTabStrip.setNonPrimaryAlpha(0.5f);
+        mPagerTabStrip.setNonPrimaryAlpha(0.4f);
         mPagerTabStrip.setDrawFullUnderline(false);
-        mPagerTabStrip.setTabIndicatorColorResource(R.color.holo_secondary);
+        mPagerTabStrip.setTabIndicatorColor(highlightColor);
         mPagerTabStrip.setTextColor(highlightColor);
 
         setHomeTitle(mCurrentPage);
