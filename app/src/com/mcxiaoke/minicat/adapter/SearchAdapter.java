2f331871f124640e3a7597249b2cbc85022c48f0
mcxiaoke <mcxiaoke@gmail.com>
2014-12-02 12:37:13 +0800

    tweak search trending ui

--- app/src/com/mcxiaoke/minicat/adapter/SearchAdapter.java
+++ app/src/com/mcxiaoke/minicat/adapter/SearchAdapter.java
@@ -13,54 +13,62 @@ import java.util.List;
 
 public class SearchAdapter extends BaseAdapter {
 
-	private LayoutInflater mInflater;
-	private Context context;
-	private List<Search> array;
+    private LayoutInflater mInflater;
+    private Context context;
+    private List<Search> array;
 
-	public SearchAdapter(Context context, List<Search> values) {
-		super();
-		this.context = context;
-		this.mInflater = LayoutInflater.from(context);
-		array = values;
-	}
+    public SearchAdapter(Context context, List<Search> values) {
+        super();
+        this.context = context;
+        this.mInflater = LayoutInflater.from(context);
+        array = values;
+    }
 
-	@Override
-	public int getCount() {
-		return array.size();
-	}
+    @Override
+    public int getCount() {
+        return array.size();
+    }
 
-	@Override
-	public Search getItem(int position) {
-		return array.get(position);
-	}
+    @Override
+    public Search getItem(int position) {
+        return array.get(position);
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder holder;
-		if (convertView == null) {
-			convertView = mInflater.inflate(R.layout.list_item_trends, null);
-			holder = new ViewHolder(convertView);
-			convertView.setTag(holder);
-		} else {
-			holder = (ViewHolder) convertView.getTag();
-		}
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_item_trends, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
 
-		holder.name.setText(array.get(position).name);
+        final Search search = getItem(position);
+        if (search != null) {
+            holder.name.setText(search.name);
+            holder.query.setText(search.query);
+        }
+        holder.name.setText(array.get(position).name);
 
-		return convertView;
-	}
 
-	private static class ViewHolder {
-		TextView name;
+        return convertView;
+    }
 
-		public ViewHolder(View base) {
-			name = (TextView) base.findViewById(R.id.text);
-		}
-	}
+    private static class ViewHolder {
+        TextView name;
+        TextView query;
+
+        public ViewHolder(View base) {
+            name = (TextView) base.findViewById(R.id.title);
+            query = (TextView) base.findViewById(R.id.text);
+        }
+    }
 
 }
